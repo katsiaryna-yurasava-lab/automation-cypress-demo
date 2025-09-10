c117006bba169dd257ecf9f9d907802500600ba4
kateyurasova <katsiaryna.yurasava@gmail.com>
2025-09-10 16:30:56 +0200

    Configuration changes

--- cypress.config.js
+++ cypress.config.js
@@ -16,6 +16,7 @@ module.exports = defineConfig({
         setupNodeEvents(on, config) {
             // implement node event listeners here
         },
+        supportFile: false
     },
     projectId: "excciq"
 });

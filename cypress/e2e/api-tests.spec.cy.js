a426d4d0481357be0254439f0ee3b3740b724869
kateyurasova <katsiaryna.yurasava@gmail.com>
2025-09-10 08:51:33 +0200

    Added api tests

---
+++ cypress/e2e/api-tests.spec.cy.js
@@ -0,0 +1,36 @@
+import airportData from "../fixtures/airports.json"
+
+describe('Sauce demo API tests: /airports', () => {
+
+    before(function () {
+        cy.log('Send a GET request to the endpoint https://airportgap.com/api/airports')
+        cy.request('GET', 'https://airportgap.com/api/airports')
+            .then(response => {
+                cy.wrap(response).as('response')
+            })
+    })
+
+    it('Scenario 1: Verify Airport Count', function () {
+        cy.log('Verify that the response contains exactly 30 airports.')
+        expect(this.response.body.data).not.to.be.empty
+        expect(this.response.body.data.length).to.eq(30)
+    })
+
+    it('Scenario 2: Verify Specific Airports', function () {
+        cy.log(`Verify that the response includes the following airports: ${airportData.toString()}`)
+        airportData.forEach(airport => {
+            cy.log(`The response includes ${airport}`)
+            expect(this.response.body.data.some((item) => item.attributes.name === airport)).to.be.true
+        })
+    })
+})
+
+describe('Sauce demo API tests: /airports/distance', () => {
+    xit('Scenario 3: Verify Distance Between Airports', () => {
+        cy.request('POST', 'https://airportgap.com/api/airports/distance',
+            {from: 'KIX', to: 'NRT'})
+            .then(response => {
+                expect(response.body.data.attributes.kilometers > 400).to.be.true
+            })
+    })
+})
